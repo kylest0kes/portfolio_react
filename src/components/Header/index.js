8ebc05ab29f121b1188d096655b9e18aacad6f20
kylest0kes <kylestokes1551@gmail.com>
2020-08-10 20:51:33 -0400

    created header component and got displayed on page with style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import kyle from '../../images/newkyle.jpg'
+
 
 export default function Header() {
     return (
         <div>
-            
+            <img style={imgStyle} className="top-img" src={kyle} alt="Kyle"/>
+
+            <div className="w3-animate-zoom">
+                <div className="container">
+                    <h1 style={displayStyle} className="display-4">Kyle Stokes</h1>
+                </div>
+            </div>
         </div>
     )
+}
+
+const displayStyle = {
+    textAlign: 'center',
+    paddingTop: '15px'
+}
+
+const imgStyle = {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '50px',
+    marginBottom: '20px',
+    borderRadius: '20px'
 }
